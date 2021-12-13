609e74a5d6ae8e1825f3ea427e34ad06be7be5c8
RY2268 <ruoyongxu2014@gmail.com>
2021-12-13 18:02:30 +0000

    man files

--- R/multiplyLowerDiagonalBatch.R
+++ R/multiplyLowerDiagonalBatch.R
@@ -19,8 +19,7 @@
 
 
 
-multiplyLowerDiagonalBatch <- function(
-                      output, L, D, B, # output = L  D B, L lower triangular, D diagonal
+multiplyLowerDiagonalBatch <- function(output, L, D, B, # output = L  D B, L lower triangular, D diagonal
                       diagIsOne, # diagonal of L is one
                       transformD, 
                       Nglobal,
